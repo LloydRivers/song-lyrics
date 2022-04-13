28cace3e657e02b795a0a7eef0f193f761092090
LloydRivers <lloydrivers58@hotmail.com>
2022-04-13 19:41:00 +0100

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Build a database of artists, albums and songs, including their lyrics. Add a nice interface, add the ability to search by lyrics
+
+// FOR EVIDENCE: please upload a document with a link to test your app working and a link to the code repo
+
+function HomePage() {
+  const url = "https://api.lyrics.ovh/v1/artist/title";
+  return <div>HomePage</div>;
+}
+
+export default HomePage;
